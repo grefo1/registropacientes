1cf7f5c13780ef76ffe622b6a2c6062eade86a21
grefo1 <braianzandona@gmail.com>
2026-01-21 00:24:04 +0000

    funcionalidad pacientes

--- backend/src/controllers/pacientes.controllers.ts
+++ backend/src/controllers/pacientes.controllers.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
+import { findAll } from "../model/paciente.model";
 
-export const getPacientes = (req: Request, res: Response) => {
-  res.json({ message: " Lista usuarios" });
+export const getPacientes = async (req: Request, res: Response) => {
+  try {
+    const pacientes = await findAll();
+    res.json(pacientes);
+  } catch (error){
+    console.error(error);
+    res.status(500).json({error:"Error al obtener pacientes"})
+  }
+  
 }
 
 export const getInfoPacientes = (req: Request, res: Response) => {
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
+import "dotenv/config"
 import express, { json, Request, Response } from "express";
 import pacientes from "./routes/pacientes.routers";
+
 
 const app = express();
 const port: number = 3000;
@@ -7,7 +9,6 @@ const port: number = 3000;
 app.use(express.json())
 
 app.use("/user", pacientes);
-app.use("/info", pacientes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hola mundo desde back-end")
@@ -19,3 +20,4 @@ export default app;
 app.listen(port, () => {
   console.log(`Servidor corriendo en localhots:${port}`)
 })
+  
